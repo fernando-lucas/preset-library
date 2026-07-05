394eb947839e68615447c5f9413533f20f8381c4
Fernando <28110985+fernando-lucas@users.noreply.github.com>
2026-07-05 11:23:43 -0300

    Padronizando configuração de imagens de amps

--- src/data/ampImages.ts
+++ src/data/ampImages.ts
@@ -1,12 +1,13 @@
-import evh5150Img from '../assets/amps/evh-5150-iii.png'
-import deluxeReverb65V2Img from '../assets/amps/fender-65-deluxe-reverb-v2.png'
-import deluxeReverb65Img from '../assets/amps/fender-65-deluxe-reverb.png'
-import fenderDeluxeReverbImg from '../assets/amps/fender-deluxe-reverb.png'
+
 import genericAmpImg from '../assets/amps/generic-amp.webp'
-import jcm800StudioImg from '../assets/amps/marshall-jcm-800-studio.png'
+import deluxeReverb65Img from '../assets/amps/fender-65-deluxe-reverb.png'
 import jcm800Img from '../assets/amps/marshall-jcm-800.png'
-import twoRockImg from '../assets/amps/two-rock-studio-signature.png'
 import voxAc30Img from '../assets/amps/vox-ac30.png'
+import voxAc30Img2 from '../assets/amps/vox-ac30-2.png'
+import twoRockStudioSignatureImg from '../assets/amps/two-rock-studio-signature.png'
+import twoRockStudioSignatureImg2 from '../assets/amps/two-rock-studio-signature-2.png'
+import evh5150Img from '../assets/amps/evh-5150-iii.png'
+
 
 export const ampImages = [
   {
@@ -15,14 +16,9 @@ export const ampImages = [
     src: genericAmpImg,
   },
   {
-    id: 'two-rock-studio-signature',
-    label: 'Two Rock Studio Signature',
-    src: twoRockImg,
-  },
-  {
-    id: 'evh-5150-iii',
-    label: 'EVH 5150 III',
-    src: evh5150Img,
+    id: 'fender-65-deluxe-reverb',
+    label: 'Fender Deluxe Reverb 65',
+    src: deluxeReverb65Img,
   },
   {
     id: 'marshall-jcm-800',
@@ -30,30 +26,30 @@ export const ampImages = [
     src: jcm800Img,
   },
   {
-    id: 'marshall-jcm-800-studio',
-    label: 'Marshall JCM 800 Studio',
-    src: jcm800StudioImg,
-  },
-  {
-    id: 'fender-65-deluxe-reverb',
-    label: 'Fender Deluxe Reverb 65',
-    src: deluxeReverb65Img,
-  },
-  {
-    id: 'fender-65-deluxe-reverb-v2',
-    label: 'Fender Deluxe Reverb 65 V2',
-    src: deluxeReverb65V2Img,
-  },
-  {
-    id: 'fender-deluxe-reverb',
-    label: 'Fender Deluxe Reverb',
-    src: fenderDeluxeReverbImg,
-  },
-  {
     id: 'vox-ac30',
     label: 'Vox AC30',
     src: voxAc30Img,
   },
+  {
+    id: 'vox-ac30-2',
+    label: 'Vox AC30 2',
+    src: voxAc30Img2,
+  },
+  {
+    id: 'two-rock-studio-signature',
+    label: 'Two Rock Studio Signature',
+    src: twoRockStudioSignatureImg,
+  },
+  {
+    id: 'two-rock-studio-signature-2',
+    label: 'Two Rock Studio Signature 2',
+    src: twoRockStudioSignatureImg2,
+  },
+  {
+    id: 'evh-5150-iii',
+    label: 'EVH 5150 III',
+    src: evh5150Img,
+  }
 ]
 
 export const defaultAmpImage =
